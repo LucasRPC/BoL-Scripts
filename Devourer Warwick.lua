d52818d88efd4b867d7c8f8f3a32e0b239a73619
LucasRPC <lucas.rodriguez-pena@hotmail.com>
2015-08-02 14:31:27 -0300

    Update Devourer Warwick.lua

--- Devourer Warwick.lua
+++ Devourer Warwick.lua
@@ -94,7 +94,6 @@ function KillSteal()
             local q, w, e, r, dmg = GetBestCombo(enemy)
             if dmg >= enemy.health then
               if Menu.KillSteal.useQ and Q:Damage(enemy) >= enemy.health and not enemy.dead then Q:Cast(enemy) end
-              if Menu.KillSteal.useW and W:Damage(enemy) >= enemy.health and not enemy.dead then W:Cast(enemy) end
               if Menu.KillSteal.useR and R:Damage(enemy) >= enemy.health and not enemy.dead then R:Cast(enemy) end
             end
             if Menu.KillSteal.useIgnite and Ignite:IsReady() and Ignite:Damage(enemy) >= enemy.health and not enemy.dead then Ignite:Cast(enemy) end
